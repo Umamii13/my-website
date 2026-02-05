9f27f9af00076c223f2be94ab12a80a92ae1b832
Puttimet Phiwngam <forfew7139@gmail.com>
2026-02-05 15:40:25 +0700

    a

--- scrips.js
+++ scrips.js
@@ -176,13 +176,11 @@ function openprojectTab(event, tabName) {
   for (i = 0; i < projecttabContent.length; i++) {
     projecttabContent[i].style.display = "none";
   }
-  print("tapcontent" + projecttabContent);
 
   projecttabLinks = document.getElementsByClassName("project-tab-link");
   for (i = 0; i < projecttabLinks.length; i++) {
     projecttabLinks[i].classList.remove("active");
   }
-  print("taplink" + projecttabLinks);
   document.getElementById(tabName).style.display = "block";
   event.currentTarget.classList.add("active");
 }
